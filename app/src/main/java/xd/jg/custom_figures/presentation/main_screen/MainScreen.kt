59cac61e03bf8cb88a709b5d1685f5b32d36c7ab
Ant <80172256+TerrifyingAnt@users.noreply.github.com>
2024-03-28 15:17:46 +0300

    Добавлена кнопка для вспомогательного меню

--- app/src/main/java/xd/jg/custom_figures/presentation/main_screen/MainScreen.kt
+++ app/src/main/java/xd/jg/custom_figures/presentation/main_screen/MainScreen.kt
@@ -1,13 +1,24 @@
 package xd.jg.custom_figures.presentation.main_screen
 
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.rounded.Menu
+import androidx.compose.material3.Card
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import xd.jg.custom_figures.presentation.main_screen.components.CenteredInRowButton
 import xd.jg.custom_figures.presentation.main_screen.components.CurrentModelState
@@ -39,6 +50,27 @@ fun MainScreen(
 
             Resource.Status.ERROR -> {
                 Text("Сделай фоту -_-")
+            }
+        }
+        Column(verticalArrangement = Arrangement.Top) {
+            Spacer(modifier = Modifier.size(10.dp))
+            Row(horizontalArrangement = Arrangement.End, modifier = Modifier.fillMaxWidth()) {
+                Card(shape = RoundedCornerShape(5.dp), modifier = Modifier
+                    .size(30.dp)
+                    .clickable {
+
+                    }) {
+                    Column(verticalArrangement = Arrangement.Center, modifier = Modifier.fillMaxHeight()) {
+                        Row(
+                            horizontalArrangement = Arrangement.Center,
+                            modifier = Modifier.fillMaxWidth()
+                        ) {
+                            Icon(Icons.Rounded.Menu, contentDescription = "Localized description")
+                        }
+                    }
+
+                }
+                Spacer(modifier = Modifier.size(10.dp))
             }
         }
 
